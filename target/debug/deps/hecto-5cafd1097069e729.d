950ec84f9ee6325e592541bc042f09120cb432e7
RanXom <saiyedshizain@gmail.com>
2024-12-21 05:31:12 +0000

    INIT

---
+++ target/debug/deps/hecto-5cafd1097069e729.d
@@ -0,0 +1,5 @@
+/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-5cafd1097069e729: src/main.rs
+
+/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-5cafd1097069e729.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hecto.d
@@ -0,0 +1,1 @@
+/home/ranxom/dev/NotPad/hecto/target/debug/hecto: /home/ranxom/dev/NotPad/hecto/src/main.rs
