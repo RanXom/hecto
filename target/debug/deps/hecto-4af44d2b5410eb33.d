39c81adc4d86208ce02cf78dd5d08463a6871c5f
RanXom <saiyedshizain@gmail.com>
2024-12-23 19:25:39 +0000

    Tildes

--- target/debug/deps/hecto-4af44d2b5410eb33.d
+++ target/debug/deps/hecto-4af44d2b5410eb33.d
@@ -1,6 +1,7 @@
-/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-4af44d2b5410eb33: src/main.rs src/editor.rs
+/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-4af44d2b5410eb33: src/main.rs src/editor.rs src/editor/terminal.rs
 
-/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-4af44d2b5410eb33.d: src/main.rs src/editor.rs
+/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-4af44d2b5410eb33.d: src/main.rs src/editor.rs src/editor/terminal.rs
 
 src/main.rs:
 src/editor.rs:
+src/editor/terminal.rs:
--- target/debug/deps/hecto-5cafd1097069e729.d
+++
@@ -1,5 +0,0 @@
-/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-5cafd1097069e729: src/main.rs
-
-/home/ranxom/dev/NotPad/hecto/target/debug/deps/hecto-5cafd1097069e729.d: src/main.rs
-
-src/main.rs:
