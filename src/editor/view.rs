30aadb2c28f3b3fa93a6e52a0a3850437dc0556c
SShizain <saiyedshizain@gmail.com>
2024-12-27 00:36:07 +0530

    Improved Lifecycle Management and Error Handling

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -1,8 +1,6 @@
-use super::terminal::{ Position, Size, Terminal };
-use std::io::Error;
+use super::terminal::{Size, Terminal};
 mod buffer;
 use buffer::Buffer;
-
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -17,21 +15,18 @@ impl View {
         self.size = to;
         self.needs_redraw = true;
     }
-
-    fn render_line(at: usize, line_text: &str) -> Result<(), Error> {
-        Terminal::move_caret_to(Position { row: at, col: 0 })?;
-        Terminal::clear_line()?;
-        Terminal::print(line_text)?;
-        Ok(())
+    fn render_line(at: usize, line_text: &str) {
+        let result = Terminal::print_row(at, line_text);
+        debug_assert!(result.is_ok(), "Failed to render line");
     }
-    
-    pub fn render(&mut self) -> Result<(), Error> {
+
+    pub fn render(&mut self) {
         if !self.needs_redraw {
-            return Ok(());
+            return;
         }
         let Size { height, width } = self.size;
         if height == 0 || width == 0 {
-            return Ok(());
+            return;
         }
         // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
         // it's allowed to be a bit too far up or down
@@ -45,15 +40,14 @@ impl View {
                 } else {
                     line
                 };
-                Self::render_line(current_row, truncated_line)?;
+                Self::render_line(current_row, truncated_line);
             } else if current_row == vertical_center && self.buffer.is_empty() {
-                Self::render_line(current_row, &Self::build_welcome_message(width))?;
+                Self::render_line(current_row, &Self::build_welcome_message(width));
             } else {
-                Self::render_line(current_row, "~")?;
+                Self::render_line(current_row, "~");
             }
         }
         self.needs_redraw = false;
-        Ok(())
     }
 
     fn build_welcome_message(width: usize) -> String {
@@ -65,11 +59,11 @@ impl View {
         if width <= len {
             return "~".to_string();
         }
-
-        // Allowing this as idc if message is perfectly center aligned or not
-        // It can be a bit to left or right
+        // we allow this since we don't care if our welcome message is put _exactly_ in the middle.
+        // it's allowed to be a bit to the left or right.
         #[allow(clippy::integer_division)]
         let padding = (width.saturating_sub(len).saturating_sub(1)) / 2;
+
         let mut full_message = format!("~{}{}", " ".repeat(padding), welcome_message);
         full_message.truncate(width);
         full_message
--- target/debug/build/lock_api-4b5437cdd85beba5/out/autocfg_8c843161889305c1_0.ll
+++ target/debug/build/lock_api-4b5437cdd85beba5/out/autocfg_8c843161889305c1_0.ll
@@ -1,5 +1,5 @@
-; ModuleID = 'autocfg_c6878c1a38d0f505_0.d3d0c7e3dbe052e9-cgu.0'
-source_filename = "autocfg_c6878c1a38d0f505_0.d3d0c7e3dbe052e9-cgu.0"
+; ModuleID = 'autocfg_8c843161889305c1_0.4b54e6cc8aec8053-cgu.0'
+source_filename = "autocfg_8c843161889305c1_0.4b54e6cc8aec8053-cgu.0"
 target datalayout = "e-m:w-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
 target triple = "x86_64-pc-windows-msvc"
 
