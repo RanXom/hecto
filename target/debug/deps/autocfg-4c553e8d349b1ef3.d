83d29bb160861223af175820d6a0ad33b58a49bd
Saiyed Shizain <saiyedshizain@gmail.com>
2025-01-14 20:21:36 +0530

    Add Scrolling

---
+++ target/debug/deps/autocfg-4c553e8d349b1ef3.d
@@ -0,0 +1,10 @@
+C:\Users\beide\OneDrive\Documents\Softwares\hecto\target\debug\deps\libautocfg-4c553e8d349b1ef3.rmeta: C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src/lib.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\error.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\rustc.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\version.rs
+
+C:\Users\beide\OneDrive\Documents\Softwares\hecto\target\debug\deps\libautocfg-4c553e8d349b1ef3.rlib: C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src/lib.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\error.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\rustc.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\version.rs
+
+C:\Users\beide\OneDrive\Documents\Softwares\hecto\target\debug\deps\autocfg-4c553e8d349b1ef3.d: C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src/lib.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\error.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\rustc.rs C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\version.rs
+
+C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src/lib.rs:
+C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\error.rs:
+C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\rustc.rs:
+C:\Users\beide\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.4.0\src\version.rs:
---
+++ target/debug/deps/hecto.d
@@ -0,0 +1,15 @@
+C:\Users\beide\OneDrive\Documents\Softwares\hecto\target\debug\deps\hecto.exe: src/main.rs src\editor.rs src\editor\editorcommand.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\location.rs src\editor\view\line.rs
+
+C:\Users\beide\OneDrive\Documents\Softwares\hecto\target\debug\deps\hecto.d: src/main.rs src\editor.rs src\editor\editorcommand.rs src\editor\terminal.rs src\editor\view.rs src\editor\view\buffer.rs src\editor\view\location.rs src\editor\view\line.rs
+
+src/main.rs:
+src\editor.rs:
+src\editor\editorcommand.rs:
+src\editor\terminal.rs:
+src\editor\view.rs:
+src\editor\view\buffer.rs:
+src\editor\view\location.rs:
+src\editor\view\line.rs:
+
+# env-dep:CARGO_PKG_NAME=hecto
+# env-dep:CARGO_PKG_VERSION=0.1.0
