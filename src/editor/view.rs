55ca4961f42411a7d23d8a813d465e8abc4a65e9
Saiyed Shizain <saiyedshizain@gmail.com>
2025-01-17 21:15:03 +0530

    Add Simple Deletion Functionality

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -31,6 +31,8 @@ impl View {
             EditorCommand::Move(direction) => self.move_text_location(&direction),
             EditorCommand::Quit => {}
             EditorCommand::Insert(character) => self.insert_char(character),
+            EditorCommand::Backspace => self.backspace(),
+            EditorCommand::Delete => self.delete(),
         }
     }
 
@@ -48,6 +50,16 @@ impl View {
     }
 
     // region: Text Editing
+    fn backspace(&mut self) {
+        self.move_left();
+        self.delete();
+    }
+
+    fn delete(&mut self) {
+        self.buffer.delete(self.text_location);
+        self.needs_redraw = true;
+    }
+
     fn insert_char(&mut self, character: char) {
         let old_len = self
             .buffer
@@ -226,7 +238,7 @@ impl View {
     fn move_left(&mut self) {
         if self.text_location.grapheme_index > 0 {
             self.text_location.grapheme_index -= 1;
-        } else {
+        } else if self.text_location.line_index > 0 {
             self.move_up(1);
             self.move_to_end_of_line();
         }
--- target/debug/build/lock_api-4b5437cdd85beba5/out/autocfg_84e8dbc891d6208d_0.ll
+++ target/debug/build/lock_api-4b5437cdd85beba5/out/autocfg_84e8dbc891d6208d_0.ll
@@ -1,5 +1,5 @@
-; ModuleID = 'autocfg_5545cf589ea26ae7_0.4d899aab83fc6912-cgu.0'
-source_filename = "autocfg_5545cf589ea26ae7_0.4d899aab83fc6912-cgu.0"
+; ModuleID = 'autocfg_84e8dbc891d6208d_0.973569bac1537f4f-cgu.0'
+source_filename = "autocfg_84e8dbc891d6208d_0.973569bac1537f4f-cgu.0"
 target datalayout = "e-m:w-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128"
 target triple = "x86_64-pc-windows-msvc"
 
